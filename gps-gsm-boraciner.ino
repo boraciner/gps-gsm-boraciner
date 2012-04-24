0b3d96e3b7f160b25ceae50e16460118b5fbdb2b
Bora Ciner <boraciner@hotmail.com>
2012-04-24 19:15:46 +0300

    first

---
+++ gps-gsm-boraciner.ino
@@ -0,0 +1,602 @@
+#include <TinyGPS.h>
+#include <EEPROM.h>
+#include <SoftwareSerial.h>
+
+#define  inDataLENTGTH 128
+//--------------------------------------------
+// Bora Ciner -- GSM Modem Home Control System
+//--------------------------------------------
+
+TinyGPS gps;
+
+SoftwareSerial gsmSerial(2, 3); //SoftwareSerial(rxPin, txPin);
+SoftwareSerial ss(5,6);
+
+boolean debug = true;
+
+const int SICAKLIK_PORT =12; 
+
+const int ROLEPin1 = 6;     // isiklarin bagli oldugu pin
+const int ROLEPin2 = 7;     // isiklarin bagli oldugu pin
+const int ROLEPin3 = 8;     // isiklarin bagli oldugu pin
+const int ROLEPin4 = 9;     // isiklarin bagli oldugu pin
+const int ROLEPin5 = 10;     // isiklarin bagli oldugu pin
+const int ROLEPin6 = 11;     // isiklarin bagli oldugu pin
+
+
+String inData = String(inDataLENTGTH); // Allocate some space for the string
+boolean inputAvailable = false;
+String strWhichMsg = String(1);
+String NewMessageDEF = String(15);
+String MessageReadString = String(10);
+String readingFromInbox = String(10);
+String sendReadRequest = String(10);
+String PASSWORD = String();
+String RemoveCommand = String("AT+CMGD=");
+String ADMIN_PHONE_NUMBER = String(11);
+String DurumBilgisiStr = String(150);
+
+int gpsoku=true;
+
+void setup()  
+{
+  Serial.begin(9600);
+
+  ss.begin(9600);
+  gsmSerial.begin(9600);  
+  
+  pinMode(ROLEPin1, OUTPUT); 
+  pinMode(ROLEPin2, OUTPUT); 
+  pinMode(ROLEPin3, OUTPUT); 
+  pinMode(ROLEPin4, OUTPUT); 
+  pinMode(ROLEPin5, OUTPUT); 
+  pinMode(ROLEPin6, OUTPUT); 
+
+  digitalWrite(ROLEPin1,HIGH); 
+  digitalWrite(ROLEPin2,HIGH); 
+  digitalWrite(ROLEPin3,HIGH); 
+  digitalWrite(ROLEPin4,HIGH); 
+  digitalWrite(ROLEPin5,HIGH); 
+  digitalWrite(ROLEPin6,HIGH); 
+  
+
+  NewMessageDEF = "+CMTI: \"SM\",";
+  strWhichMsg = "";
+  MessageReadString = "at+cmgr=";
+  readingFromInbox = "REC UNREAD";
+
+  ReadPasswordFromEEPROM();
+  
+  Serial.print("PASSWORD =");
+  Serial.println(PASSWORD);
+  
+  if(debug)
+  Serial.println("Setup..! wait for 1 sec");
+  delay(1000);
+  Serial.print("_SS_MAX_RX_BUFF = ");
+  Serial.println(_SS_MAX_RX_BUFF);
+//  removeSms();  
+}
+
+void loop() // run over and over
+{
+  gsmSerial.listen();
+  while(gsmSerial.available())
+  {
+    inputAvailable = true;
+    int tmp = gsmSerial.read();
+    inData += (char) tmp;
+    delay(7);
+  }
+  if(inputAvailable){
+    inputAvailable=false;
+    Serial.println(inData);  
+    
+    if (inData.indexOf("VOICE") >= 0 )
+    {
+        //Serial.println("telefon caliyor");
+        //AramayiMesguleCevir();    
+        //removeSms();
+        TAKEGPSDATA();
+    }
+  }
+  inData="";
+    
+  
+}
+void printGPSDATA(){
+  
+    float flat, flon;
+    unsigned long age;
+    gps.f_get_position(&flat, &flon, &age);
+    Serial.print("LAT=");
+    Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+    Serial.print(" LON=");
+    Serial.println(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+}
+
+void TAKEGPSDATA(){  
+ if(debug)Serial.println("---->TAKEGPSDATA"); 
+    ss.listen(); 
+    delay(100);
+    
+    for (unsigned long start = millis(); millis() - start < 1000;)
+    {
+      while (ss.available())
+      {
+        char c = ss.read();
+        Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+        if (gps.encode(c)) // Did a new valid sentence come in?
+          { 
+            printGPSDATA();
+          }
+      }
+    }
+    gsmSerial.listen();  
+ if(debug)Serial.println("<----TAKEGPSDATA");     
+}
+
+void processData(){
+  Serial.println("---->processData");  
+  Serial.println(inData);  
+  /*
+  if(thereIsNewMessage())
+  { // yeni mesaj var
+    Serial.println("yeni mesaj var");
+    String sendReadRequest = "";
+    sendReadRequest += MessageReadString;
+    
+    int indexofMsgStr = inData.indexOf(NewMessageDEF);
+    indexofMsgStr += NewMessageDEF.length();
+    strWhichMsg = inData.substring(indexofMsgStr , indexofMsgStr+1); 
+    sendReadRequest +=strWhichMsg;
+    
+    Serial.print("gonderilen komut = ");
+    Serial.println(sendReadRequest);
+    
+    gsmSerial.println(sendReadRequest); // xxxx inci mesaji oku
+  }else if(IsRinging())
+  { // telefon caliyor
+    Serial.println("telefon caliyor");
+    //AramayiMesguleCevir();    
+    //removeSms();
+    TAKEGPSDATA();
+  }/*else if(ReadFromInbox())
+  {
+    takeMessageBody();
+    processMessageBody();
+    removeSms();
+  }else
+  {
+    if(debug)Serial.println("ELSE");  
+  }
+  */
+  Serial.println("<----processData");  
+}
+
+void AramayiMesguleCevir(){
+  gsmSerial.println("AT H");
+  delay(50);
+}
+
+
+void takeMessageBody(){
+if(debug)Serial.println("---->takeMessageBody");  
+int index_of_NewLine = 0;
+
+int indexofPhoneStr = inData.indexOf("\",\"+9");
+indexofPhoneStr += 5;
+ADMIN_PHONE_NUMBER = inData.substring(indexofPhoneStr , indexofPhoneStr+11); 
+if(debug)
+{
+  Serial.print("ADMIN PHONE NUMBER ");
+  Serial.println(ADMIN_PHONE_NUMBER);
+}
+  index_of_NewLine = inData.indexOf("REC UNREAD");  
+  index_of_NewLine += 53;
+  inData = inData.substring(index_of_NewLine,index_of_NewLine+29);
+
+if(debug)
+{
+  Serial.println("Message Body ===>");
+  Serial.println(inData);
+  Serial.println("<----takeMessageBody");   
+}
+}
+
+void processMessageBody(){
+
+if(debug)Serial.println("---->processMessageBody");  
+boolean sendstatus = false;
+if(passwordIsCorrect()){
+    if(DurumSoruyor())
+    {
+      if(debug)Serial.println("Durum Soruyor !");   
+      sendstatus = true;            
+    }
+    else if(SifreDegistir())
+    {
+      //YENISIFRE12345678 20012001           
+      PASSWORD = inData.substring(9,17);
+      if(debug)
+      {
+        Serial.println("New Password =>");   
+        Serial.println(PASSWORD);   
+      }
+      WriteNewPasswordToEEPROM();
+    }
+    
+    /*********************************/
+    else if(ROLE1AC()){
+      digitalWrite(ROLEPin1,LOW);     
+      sendstatus = true;            
+    }else if(ROLE1KAPAT()){
+      digitalWrite(ROLEPin1,HIGH);     
+      sendstatus = true; 
+    }
+    else if(ROLE2AC()){
+      digitalWrite(ROLEPin2,LOW);     
+      sendstatus = true;            
+    }
+    else if(ROLE2KAPAT()){
+      digitalWrite(ROLEPin2,HIGH);     
+      sendstatus = true; 
+    }
+    else if(ROLE3AC()){
+      digitalWrite(ROLEPin3,LOW);     
+      sendstatus = true;            
+    }
+    else if(ROLE3KAPAT()){
+      digitalWrite(ROLEPin3,HIGH);     
+      sendstatus = true; 
+    }
+    else if(ROLE4AC()){
+      digitalWrite(ROLEPin4,LOW);     
+      sendstatus = true;            
+    }
+    else if(ROLE4KAPAT()){
+      digitalWrite(ROLEPin4,HIGH);     
+      sendstatus = true; 
+    }
+    else if(ROLE5AC()){
+      digitalWrite(ROLEPin5,LOW);     
+      sendstatus = true;            
+    }
+    else if(ROLE5KAPAT()){
+      digitalWrite(ROLEPin5,HIGH);     
+      sendstatus = true; 
+    }
+    else if(ROLE6AC()){
+      digitalWrite(ROLEPin6,LOW);     
+      sendstatus = true;            
+    }
+    else if(ROLE6KAPAT()){
+      digitalWrite(ROLEPin6,HIGH);     
+      sendstatus = true; 
+    }
+    else if(SicaklikSoruyor()){
+      int Temp_Home = int(Sicaklik(analogRead(SICAKLIK_PORT)));
+      
+      if(debug)
+      {
+        Serial.print("SICAKLIK=");   
+        Serial.println(Temp_Home);  
+      }
+    } 
+    /*********************************/      
+    if(sendstatus){
+      DurumBilgisiGonder();
+    }
+}
+else
+{
+  if(debug)Serial.println("Hatali Sifre!!");     
+}
+
+if(debug)Serial.println("<----processMessageBody");    
+}
+
+/***********DURUM BILGISI GONDER*********/
+/*****************START*******************/
+void DurumBilgisiGonder(){
+  String atSendNumber = String(23);
+  DurumBilgisiStr="ROLE BILGILERI 1=";
+  if(digitalRead(ROLEPin1)==HIGH)
+     DurumBilgisiStr+="KAPALI";
+  else
+     DurumBilgisiStr+="ACIK";
+  DurumBilgisiStr+=" 2=";
+  
+  if(digitalRead(ROLEPin2)==HIGH)
+     DurumBilgisiStr+="KAPALI";
+  else
+     DurumBilgisiStr+="ACIK";
+  
+  DurumBilgisiStr+=" 3=";
+  if(digitalRead(ROLEPin3)==HIGH)
+     DurumBilgisiStr+="KAPALI";
+  else
+     DurumBilgisiStr+="ACIK"; 
+  
+  DurumBilgisiStr+=" 4=";
+  if(digitalRead(ROLEPin4)==HIGH)
+     DurumBilgisiStr+="KAPALI";
+  else
+     DurumBilgisiStr+="ACIK";
+  
+  DurumBilgisiStr+=" 5=";
+  if(digitalRead(ROLEPin5)==HIGH)
+     DurumBilgisiStr+="KAPALI";
+  else
+     DurumBilgisiStr+="ACIK";
+        
+  DurumBilgisiStr+=" 6=";
+  if(digitalRead(ROLEPin6)==HIGH)
+     DurumBilgisiStr+="KAPALI";
+  else
+     DurumBilgisiStr+="ACIK";
+
+  /////////////SEND SMS////////////
+  gsmSerial.println("AT+CMGF=1");
+  
+  atSendNumber = "AT+CMGS=\"+9";
+  atSendNumber += ADMIN_PHONE_NUMBER;
+  atSendNumber += "\"";
+  
+  gsmSerial.println(atSendNumber);
+  gsmSerial.println(DurumBilgisiStr);
+  gsmSerial.write(26);
+
+}
+
+
+void removeSms(){
+if(debug)Serial.println("---->removeSms");  
+gsmSerial.println("AT+CMGD=1,4");
+delay(100);
+gsmSerial.write(26);
+if(debug)Serial.println("<----removeSms");
+}
+
+
+/***********BOOLEAN IF ELSE FUNCTIONS*********/
+/*****************START***********************/
+int DurumSoruyor()
+{
+  if (inData.indexOf("DURUM") >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+/**************************************************************/
+int ROLE1AC()
+{
+  if (inData.indexOf("ROLE1AC") >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+int ROLE2AC()
+{
+  if (inData.indexOf("ROLE2AC") >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+int ROLE3AC()
+{
+  if (inData.indexOf("ROLE3AC") >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+int ROLE4AC()
+{
+  if (inData.indexOf("ROLE4AC") >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+int ROLE5AC()
+{
+  if (inData.indexOf("ROLE5AC") >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+int ROLE6AC()
+{
+  if (inData.indexOf("ROLE6AC") >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+int ROLE1KAPAT()
+{
+  if (inData.indexOf("ROLE1KAPAT") >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+int ROLE2KAPAT()
+{
+  if (inData.indexOf("ROLE2KAPAT") >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+int ROLE3KAPAT()
+{
+  if (inData.indexOf("ROLE3KAPAT") >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+int ROLE4KAPAT()
+{
+  if (inData.indexOf("ROLE4KAPAT") >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+int ROLE5KAPAT()
+{
+  if (inData.indexOf("ROLE5KAPAT") >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+int ROLE6KAPAT()
+{
+  if (inData.indexOf("ROLE6KAPAT") >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+/***************************************************************/
+int thereIsNewMessage()
+{
+  if (inData.indexOf(NewMessageDEF) >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+int passwordIsCorrect()
+{
+  if (inData.indexOf(PASSWORD) >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+int IsRinging()
+{
+  if (inData.indexOf("CRING: VOICE") >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+int SifreDegistir()
+{
+  if (inData.indexOf("YENISIFRE") >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+int SicaklikSoruyor()
+{
+  if (inData.indexOf("SICAKLIK") >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+int ReadFromInbox()
+{
+  if (inData.indexOf(readingFromInbox) >= 0 )
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+void WriteNewPasswordToEEPROM(){
+  for( int cnt = 0; cnt < 8; cnt ++)
+    EEPROM.write(cnt, PASSWORD.charAt(cnt));
+}
+
+void ReadPasswordFromEEPROM(){
+  PASSWORD="";
+  for( int cnt = 0; cnt < 8; cnt ++)
+    PASSWORD += (char) EEPROM.read(cnt);
+}
+
+// KELVIN DEN CELCIUS A DONUSTUR
+double Sicaklik(int RawADC) {
+double Temp;
+Temp = log(((10240000/RawADC) - 10000));
+Temp = 1 / (0.001129148 + (0.000234125 * Temp) + (0.0000000876741 * Temp * Temp * Temp));
+Temp = Temp - 273.15; 
+return Temp;
+}
+
+
+//************************************************
+
+
